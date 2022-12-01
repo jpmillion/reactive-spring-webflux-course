cb3d098431241ec2979c5efd7a797b0c4ce032fd
John Million <john.million@rtslabs.com>
2022-12-01 18:48:28 -0500

    Define namesFlux test

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,25 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
+    @Test
+    void nameFlux() {
+
+        //when
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+
+    }
 }
